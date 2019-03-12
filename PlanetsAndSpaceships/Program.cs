a6241928ff96189f8bd376a03d9fd1a43302a27b
Shane D Wilson <me@shanedwilson.com>
2019-03-11 20:19:55 -0500

    Add project files.

---
+++ PlanetsAndSpaceships/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PlanetsAndSpaceships
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
